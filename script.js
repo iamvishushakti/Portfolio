0d19c3f111c31c408e34507f8c429dc598825524
vishal kumar <46447233+iamvishushakti@users.noreply.github.com>
2026-02-02 22:17:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 
-        /* SLIDE CONTROL */
+        // /* SLIDE CONTROL */
 
-        let current = 0;
-        const slides = document.querySelectorAll('.slide');
-        let lock = false;
-        function show(i) {
-            slides[current].classList.remove('active');
-            slides[current].scrollTop = 0;
-            current = (i + slides.length) % slides.length;
-            slides[current].classList.add('active');
-        }
+        // let current = 0;
+        // const slides = document.querySelectorAll('.slide');
+        // let lock = false;
+        // function show(i) {
+        //     slides[current].classList.remove('active');
+        //     slides[current].scrollTop = 0;
+        //     current = (i + slides.length) % slides.length;
+        //     slides[current].classList.add('active');
+        // }
 
-        function goTo(i) {
-            show(i);
-        }
+        // function goTo(i) {
+        //     show(i);
+        // }
 
 
-        /* SMART SCROLL + SLIDE */
+        // /* SMART SCROLL + SLIDE */
 
-        window.addEventListener('wheel', e => {
-            const slide = slides[current];
-            const atTop = slide.scrollTop === 0;
-            const atBottom = Math.ceil(slide.scrollTop + slide.clientHeight) >= slide.scrollHeight;
-            if (lock) return;
-            if (e.deltaY > 0 && atBottom) {
-                lock = true;
-                show(current + 1);
-                setTimeout(() => lock = false, 900);
-            }
-            else if (e.deltaY < 0 && atTop) {
-                lock = true;
-                show(current - 1);
-                setTimeout(() => lock = false, 900);
-            }
-        }
+        // window.addEventListener('wheel', e => {
+        //     const slide = slides[current];
+        //     const atTop = slide.scrollTop === 0;
+        //     const atBottom = Math.ceil(slide.scrollTop + slide.clientHeight) >= slide.scrollHeight;
+        //     if (lock) return;
+        //     if (e.deltaY > 0 && atBottom) {
+        //         lock = true;
+        //         show(current + 1);
+        //         setTimeout(() => lock = false, 900);
+        //     }
+        //     else if (e.deltaY < 0 && atTop) {
+        //         lock = true;
+        //         show(current - 1);
+        //         setTimeout(() => lock = false, 900);
+        //     }
+        // }
 
-            , {
-                passive: true
-            }
+        //     , {
+        //         passive: true
+        //     }
 
-        );
+        // );
 
-        /* TYPING EFFECT */
+        // /* TYPING EFFECT */
 
-        const nameText = "Vishal Kumar";
-        let i = 0;
-        const target = document.getElementById("typingName");
-        (function type() {
-            if (i < nameText.length) {
-                target.textContent += nameText.charAt(i++);
-                setTimeout(type, 120);
-            }
-        }
+        // const nameText = "Vishal Kumar";
+        // let i = 0;
+        // const target = document.getElementById("typingName");
+        // (function type() {
+        //     if (i < nameText.length) {
+        //         target.textContent += nameText.charAt(i++);
+        //         setTimeout(type, 120);
+        //     }
+        // }
 
-        )();
+        // )();
